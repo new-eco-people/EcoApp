155e57c897a1fb19a9a57de82709311a4ae2f0e2
Eco World <49808864+new-eco-people@users.noreply.github.com>
2019-04-20 10:50:38 +0100

    Update WebCustomExceptionFilter.cs

--- Web.Ui/Filters/WebCustomExceptionFilter.cs
+++ Web.Ui/Filters/WebCustomExceptionFilter.cs
@@ -51,33 +51,4 @@ namespace Web.Ui.Filters
             });
         }
     }
-
-    public class ValidateModelStateAttribute : ActionFilterAttribute
-    {
-        public override void OnActionExecuting(ActionExecutingContext context)
-        {
-            if (!context.ModelState.IsValid)
-            {
-                var errors = context.ModelState.Values.Where(v => v.Errors.Count > 0)
-                        .SelectMany(v => v.Errors)
-                        .Select(v => v.ErrorMessage)
-                        .ToList();
-
-                // var ValidationFailureErrors = new ValidationFailure("Validation", errors);
-
-                // throw new CustomValidationException(new List<FluentValidation.Results.ValidationFailure>());
-
-                var responseObj = new
-                {
-                    // Message = "Bad Request",
-                    Errors = errors                    
-                };
-
-                context.Result = new JsonResult(responseObj)
-                {
-                    StatusCode = 400
-                };
-            }
-        }
-    }
 }
